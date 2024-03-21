5be3161634658454e384ba485d29237320ee820c
midhun2208 <midhun.sm343@gmail.com>
2024-03-21 15:31:25 +0530

    By midhun

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AdminDashboard from './Admin/AdminDashboard'
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/admin-dash' element={<AdminDashboard/>}/>
+      </Routes>
     </div>
   );
 }
