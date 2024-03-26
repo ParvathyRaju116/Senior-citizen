2076ccebce3c72709c69a1ff3e4e097813628825
ajinsapa <ajinsaashik008@gmail.com>
2024-03-26 23:20:25 +0530

    nexxt

--- src/Admin/AdminGraph/ServiceGraph.js
+++ src/Admin/AdminGraph/ServiceGraph.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useEffect } from "react";
-import { ChartComponent, ColumnSeries, Category, DataLabel, Tooltip, SeriesDirective, SeriesCollectionDirective, Inject } from '@syncfusion/ej2-react-charts';
 
 function ServiceGraph() {
   return (
