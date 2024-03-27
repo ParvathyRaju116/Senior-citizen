2173b4fccc3ac368cd84ef42f9e60a702636a101
AryamolMs <aryamolsimi@gmail.com>
2024-03-27 14:04:57 +0530

    adminchart

--- src/Admin/AdminGraph/ServiceGraph.js
+++ src/Admin/AdminGraph/ServiceGraph.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 function ServiceGraph() {
   return (
     <div>
-      
+      <BarChart
+        xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+        series={[{ data: [4, 3, 5] }]} // Keep only one set of data
+        width={300}
+        height={300}
+      />
     </div>
-  )
+  );
 }
 
-export default ServiceGraph
+export default ServiceGraph;
