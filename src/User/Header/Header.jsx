4185376157f4b3bfefc75c6450fa5f890e5321ce
midhun2208 <midhun.sm343@gmail.com>
2024-04-01 11:50:48 +0530

    Midhun P M

--- src/User/Header/Header.jsx
+++ src/User/Header/Header.jsx
@@ -2,43 +2,44 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Avatar } from '@mui/material';
-import './Headre.css'
+import './Headre.css';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
-
 function Header() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handleLogout = ()=>{
-
-    sessionStorage.clear()
-    navigate("/")
+  const handleLogout = () => {
+    sessionStorage.clear();
+    navigate('/');
     Swal.fire({
-      title: "Logged out",
-      icon: "success"
+      title: 'Logged out',
+      icon: 'success',
     });
-  }
+  };
 
   return (
     <>
-      <Navbar expand="lg"  style={{height:'70px'}}>
+      <Navbar expand="lg" style={{ height: '70px' }}>
         <Container className='header'>
           <Navbar.Brand className='fw-bolder'>LiveCare</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-          <Dropdown className='ms-auto'>
+            <Dropdown className='ms-auto'>
               <Dropdown.Toggle variant="link" id="dropdown-basic">
-                <Avatar alt="Remy Sharp" src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" />
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png"
+                />
               </Dropdown.Toggle>
               <Dropdown.Menu>
-              <Dropdown.Item><Link to={'/user-home'} style={{textDecoration:'none',color:'black'}}> Home</Link></Dropdown.Item>
-              <Dropdown.Item><Link to={'/user-profile'} style={{textDecoration:'none',color:'black'}}> Profile</Link></Dropdown.Item>
-              <Dropdown.Item><Link to={'/user-feedback'} style={{textDecoration:'none',color:'black'}}> Feedback</Link></Dropdown.Item>
-                <Dropdown.Item><Link to={'/user-complaints'} style={{ textDecoration: 'none', color: 'black' }}> Complaints</Link></Dropdown.Item>
-                <Dropdown.Item><Link to={'/user-bookings'} style={{textDecoration:'none',color:'black'}}> My Bookings</Link></Dropdown.Item>
-              <Dropdown.Item  onClick={handleLogout}>Logout</Dropdown.Item>
+                <Dropdown.Item as={Link} to='/user-home' style={{ textDecoration: 'none', color: 'black' }}>Home</Dropdown.Item>
+                <Dropdown.Item as={Link} to='/user-profile' style={{ textDecoration: 'none', color: 'black' }}>Profile</Dropdown.Item>
+                <Dropdown.Item as={Link} to='/user-feedback' style={{ textDecoration: 'none', color: 'black' }}>Feedback</Dropdown.Item>
+                <Dropdown.Item as={Link} to='/user-complaints' style={{ textDecoration: 'none', color: 'black' }}>Complaints</Dropdown.Item>
+                <Dropdown.Item as={Link} to='/user-bookings' style={{ textDecoration: 'none', color: 'black' }}>My Bookings</Dropdown.Item>
+                <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </Navbar.Collapse>
