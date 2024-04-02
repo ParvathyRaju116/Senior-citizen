d6d0994b13098c8bfadf049d38e5b723aa738a12
midhun2208 <midhun.sm343@gmail.com>
2024-04-02 12:26:18 +0530

    Midhun P M

--- src/User/Profile/Profile.js
+++ src/User/Profile/Profile.js
@@ -1,162 +1,159 @@
-import React from 'react'
-import Header from '../Header/Header'
-import { useEffect, useState } from 'react';
-import Collapse from 'react-bootstrap/Collapse';
-import { editApi } from '../../Services/allApi';
-import Swal from 'sweetalert2';
-
+import React from "react";
+import Header from "../Header/Header";
+import { useEffect, useState } from "react";
+import Collapse from "react-bootstrap/Collapse";
+import { editApi } from "../../Services/allApi";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 function Profile() {
-    const [open, setOpen] = useState(false);
-    const [isUpdate,setIsUpdate]= useState(false)
+  const [open, setOpen] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
 
-  const [profile,setProfile] = useState({
-    username:"",
-    email:"",
-    password:"",
-    number:"",
-    address:""
-  })
- 
-  const handleProfile = async()=>{
-    const {username,email,password,number,address} = profile
+  const [profile, setProfile] = useState({
+    id: "",
+    username: "",
+    email: "",
+    password: "",
+    number: "",
+    address: "",
+  });
 
-    if(!username || !email || !password || !number || !address){
-        Swal({
-            title: "Please fill this form completely",
-            icon: "warning"
-          });
-    }
-    else{const reqBody = new FormData();
-
-        reqBody.append("username", username);
-        reqBody.append("email", email);
-        reqBody.append("password", password);
-        reqBody.append("number", number);
-        reqBody.append("address", address);
-       
-    
-        try {
-          const result = await editApi(reqBody);
-          console.log(result);
-          if (result.status === 200) {
-            Swal.fire({
-              title: "Successfully Updated!",
-              icon: "success"
-            });
-            sessionStorage.getItem("existingUser",JSON.stringify(result.data))
-            setIsUpdate(true)
-            setProfile(result.data);
-          } else {
-            console.log(result.response.data);
-          }
-        } catch (error) {
-          console.error(error);
-          Swal.fire({
-            title: "An error occurred. Please try again later!",
-            icon: "warning"
-          });
-        }
-      }
-    }; 
-  
-
-/* const handleProfile = async () => {
-  const { id, username, email, password, number, address } = profile;
-
-  if (!username || !email || !password || !number || !address) {
-    Swal.fire({
-      title: "Please fill this form completely",
-      icon: "warning"
-    });
-  } else {
-    const userData = {
-      username,
-      email,
-      password,
-      number,
-      address
-    };
+  const handleProfile = async () => {
+    const reqBody = new FormData();
+    const { username, email, password, number, address } = profile;
+    reqBody.append("username", username);
+    reqBody.append("email", email);
+    reqBody.append("password", password);
+    reqBody.append("number", number);
+    reqBody.append("address", address);
 
     try {
-      const response = await axios.put(`http://localhost:5000/${id}`, userData);
-      console.log(response);
-      if (response.status === 200) {
-        Swal.fire({
-          title: "Successfully Updated!",
-          icon: "success"
-        });
+      const response = await axios.post(
+        `http://localhost:5000/editUser/${profile.id}`,
+        reqBody
+      );
 
-        setProfile(response.data);
-      } else {
-        console.log(response.data);
+      if (response.status >= 200 && response.status <= 300) {
+        console.log(response);
+        Swal.fire({
+          title: "Updated Succesfully",
+          icon: "success",
+        });
+        sessionStorage.setItem(
+          "existingUser",
+          JSON.stringify(response.data.updatedUser)
+        );
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
       Swal.fire({
-        title: "An error occurred. Please try again later!",
-        icon: "warning"
+        title: "Something went wrong",
+        icon: "warning",
       });
     }
-  }
-}; */
+  };
 
-    
+  useEffect(() => {
+    if (sessionStorage.getItem("existingUser")) {
+      const user = JSON.parse(sessionStorage.getItem("existingUser"));
+      if (user) {
+        setProfile({
+          ...profile,
+          id: user._id || "",
+          username: user.username || "",
+          email: user.email || "",
+          address: user.address || "",
+          number: user.number || "",
+          password: user.password || "",
+        });
+      }
+    }
+  }, [isUpdate]);
 
-      
-    useEffect(() => {
-        if (sessionStorage.getItem("existingUser")) {
-          const user = JSON.parse(sessionStorage.getItem("existingUser"));
-          if (user) {
-            setProfile({
-              ...profile,
-              username: user.username || "",
-              email: user.email || "",
-              address:user.address || "",
-              number:user.number || "",
-              password: user.password || "",
-              
-            });
-          }
-        }
-      }, [isUpdate]);
-
-  console.log(profile);
 
   return (
     <>
-     <Header /> 
-     <div className='d-flex justify-content-center pt-5'>
-         <div className='card shadow p-5 w-50 mt-5'>
-            <div className='d-flex justify-content-between'>
-               <h3> Profile</h3>
-               <button className='btn 'onClick={() => setOpen(!open)} ><i class="fa-solid fa-arrow-up-from-bracket fa-rotate-180 ms-auto"></i></button>
-            </div>
-           <Collapse in={open} >
-              <div className='row justify-content-center'>
-                  
-                  <div className='mb-3'>
-                    <input type="text" placeholder='username' className='form-control mt-3' value={profile.username} onChange={(e)=>setProfile({...profile,username:e.target.value})}/>
-                    </div>
-                    <div className='mb-3'>
-                    <input type="text" placeholder='email' className='form-control mt-3' value={profile.email} onChange={(e)=>setProfile({...profile,email:e.target.value})}/>
-                    </div>
-                    <div className='mb-3'>
-                    <input type="text" placeholder='phone no' className='form-control mt-3' value={profile.number} onChange={(e)=>setProfile({...profile,number:e.target.value})}/>
-                    </div>
-                    <div className='mb-3'>
-                    <input type="text" placeholder='Address' className='form-control mt-3' value={profile.address} onChange={(e)=>setProfile({...profile,address:e.target.value})}/>
-                    </div>
-                    <div className='mb-3'>
-                    <input type="text" placeholder='Password' className='form-control mt-3' value={profile.password} onChange={(e)=>setProfile({...profile,password:e.target.value})}/>
-                    </div>
-                  <button type='button' className='btn btn-warning mt-3'  onClick={handleProfile} >Update</button>
+      <Header />
+      <div className="d-flex justify-content-center pt-5">
+        <div className="card shadow p-5 w-50 mt-5">
+          <div className="d-flex justify-content-between">
+            <h3> Profile</h3>
+            <button className="btn " onClick={() => setOpen(!open)}>
+              <i class="fa-solid fa-arrow-up-from-bracket fa-rotate-180 ms-auto"></i>
+            </button>
+          </div>
+          <Collapse in={open}>
+            <div className="row justify-content-center">
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="username"
+                  className="form-control mt-3"
+                  value={profile.username}
+                  onChange={(e) =>
+                    setProfile({ ...profile, username: e.target.value })
+                  }
+                />
               </div>
-           </Collapse>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="form-control mt-3"
+                  value={profile.email}
+                  disabled
+                  onChange={(e) =>
+                    setProfile({ ...profile, email: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="phone no"
+                  className="form-control mt-3"
+                  value={profile.number}
+                  onChange={(e) =>
+                    setProfile({ ...profile, number: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="Address"
+                  className="form-control mt-3"
+                  value={profile.address}
+                  onChange={(e) =>
+                    setProfile({ ...profile, address: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="Password"
+                  className="form-control mt-3"
+                  value={profile.password}
+                  onChange={(e) =>
+                    setProfile({ ...profile, password: e.target.value })
+                  }
+                />
+              </div>
+              <button
+                type="button"
+                className="btn btn-warning mt-3"
+                onClick={handleProfile}
+              >
+                Update
+              </button>
+            </div>
+          </Collapse>
         </div>
-     </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
