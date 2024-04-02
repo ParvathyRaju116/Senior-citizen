4efd8cddf89a55c14773b73f917d83a758a354b1
ajinsapa <ajinsaashik008@gmail.com>
2024-04-02 23:36:39 +0530

    bookings2

--- src/Admin/AdminBookings/Bookings.js
+++ src/Admin/AdminBookings/Bookings.js
@@ -1,51 +1,53 @@
 // Bookings.jsx
-import React from 'react';
-import './Bookings.css';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
+import React from "react";
+import "./Bookings.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import List from "./List";
 
 function Bookings() {
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    color: '#E6CCB2',
-   
+    color: "#E6CCB2",
+
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
-      width: '80%',
+      width: "80%",
     },
   }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   }));
-  
+
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    width: '100%',
-    '& .MuiInputBase-input': {
+    color: "inherit",
+    width: "100%",
+    "& .MuiInputBase-input": {
       padding: theme.spacing(1, 1, 1, 0),
       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
+      transition: theme.transitions.create("width"),
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
         },
       },
     },
@@ -53,10 +55,10 @@ function Bookings() {
 
   return (
     <div>
-      <div className='m-5 p-5'>
-        <h1 className='text-center book1'>Booking List</h1>
+      <div className="m-5 p-5" style={{width:"100%"}}>
+        <h1 className="text-center book1">Booking List</h1>
         <Box sx={{ flexGrow: 1 }} className="m-5">
-          <AppBar position="static" sx={{ backgroundColor: '#9C6644' }}> 
+          <AppBar position="static" sx={{ backgroundColor: "#9C6644" }}>
             <Toolbar>
               <IconButton
                 size="large"
@@ -64,14 +66,17 @@ function Bookings() {
                 color="inherit"
                 aria-label="open drawer"
                 sx={{ mr: 2 }}
-              >
-                
-              </IconButton>
+              ></IconButton>
               <Typography
                 variant="h6"
                 noWrap
                 component="div"
-                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, color: 'beige' }}              >
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "none", sm: "block" },
+                  color: "beige",
+                }}
+              >
                 Search Bookings
               </Typography>
               <Search>
@@ -80,13 +85,14 @@ function Bookings() {
                 </SearchIconWrapper>
                 <StyledInputBase
                   placeholder="Search…"
-                  inputProps={{ 'aria-label': 'search' }}
-                  color='beige'
+                  inputProps={{ "aria-label": "search" }}
+                  color="beige"
                 />
               </Search>
             </Toolbar>
           </AppBar>
         </Box>
+        <List></List>
       </div>
     </div>
   );
---
+++ src/Admin/AdminBookings/List.js
@@ -0,0 +1,45 @@
+import { Container } from "@mui/material";
+import React from "react";
+import Table from 'react-bootstrap/Table';
+
+function List() {
+  return <Container>
+
+
+<Table striped bordered hove className="ms-5"  style={{width:"90%",border :"3px brown solid"}}>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+
+
+
+
+  </Container>;
+}
+
+export default List;
