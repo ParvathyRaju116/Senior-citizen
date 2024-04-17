57c6c58cad3d0c38bfac80079949213280da7b2a
midhun2208 <midhun.sm343@gmail.com>
2024-04-17 16:51:57 +0530

    Midhun

--- src/Admin/AdminBookings/List.js
+++ src/Admin/AdminBookings/List.js
@@ -4,12 +4,15 @@ import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { getBookingAdminApi } from "../../Services/allApi";
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap";
+import axios from "axios";
+import baseurl from "../../Services/baseurl";
+import Swal from "sweetalert2";
 
 function formatDate(dateString) {
   const date = new Date(dateString);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero based
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are zero based
   const year = date.getFullYear();
   return `${day}-${month}-${year}`;
 }
@@ -34,6 +37,32 @@ function List() {
   useEffect(() => {
     getBookings();
   }, []);
+
+  const handleResponse = async (item) => {
+    try {
+      const id = { id: item };
+      const response = await axios.post(
+        `${baseurl}/maternalcare/admin/primarybooking/accept`,
+        id
+      );
+      if (response.status >= 200 && response.status <= 300) {
+        Swal.fire({
+          title: "Approved",
+          text: "Booking Approved Succesfuly",
+          icon: "success",
+        });
+        console.log(response);
+        getBookings();
+      }
+    } catch (error) {
+      Swal.fire({
+        title: "Not Approved",
+        text: "Booking Not Approved",
+        icon: "warning",
+      });
+      console.log(error);
+    }
+  };
 
   return (
     <Container>
@@ -82,9 +111,17 @@ function List() {
                     <div className="ms-5">
                       {" "}
                       <Stack direction="row" spacing={2}>
-                        <Button variant="contained" color="success">
-                          Confirm
-                        </Button>
+                        {i?.adminStatus === "approved" ? (
+                          <h6 className="text-success">Approved</h6>
+                        ) : (
+                          <Button
+                            variant="contained"
+                            color="success"
+                            onClick={() => handleResponse(i?._id)}
+                          >
+                            Confirm
+                          </Button>
+                        )}
                       </Stack>
                     </div>
                   </td>
