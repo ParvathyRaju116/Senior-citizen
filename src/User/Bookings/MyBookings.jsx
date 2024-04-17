156c9bb6707405b63767fa5780729d1b32e57ee0
midhun2208 <midhun.sm343@gmail.com>
2024-04-17 18:01:09 +0530

    Midhun P M

--- src/User/Bookings/MyBookings.jsx
+++ src/User/Bookings/MyBookings.jsx
@@ -1,88 +1,118 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../Header/Header';
-import { Container, Col } from 'react-bootstrap';
-import './mybookings.css';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
-import Payment from './Payment';
-import Swal from 'sweetalert2';
-import { getUnpaidbillsApi } from '../../Services/allApi';
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import { Container, Col, Row } from "react-bootstrap";
+import "./mybookings.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
+import Payment from "./Payment";
+import Swal from "sweetalert2";
+import { getUnpaidbillsApi } from "../../Services/allApi";
+import axios from "axios";
+import baseurl from "../../Services/baseurl";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 function MyBookings() {
+  const [total,setTotal] = useState()
   const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
+  const handleOpen = (item,amount) => {
+    const id = {id:item}
+    setTotal(amount)
+
+    setOpen(true);
+  };
   const handleClose = () => setOpen(false);
-  const token = sessionStorage.getItem("token")
+  const token = sessionStorage.getItem("token");
+  const [unpaid, setUnpaid] = useState([]);
 
   const [feedback, setFeedback] = useState({ rating: 5, comments: "" });
   const [hover, setHover] = useState(-1);
   const labels = {
-    1: 'Useless',
-    2: 'Poor',
-    3: 'Ok',
-    4: 'Good',
-    5: 'Excellent',
+    1: "Useless",
+    2: "Poor",
+    3: "Ok",
+    4: "Good",
+    5: "Excellent",
   };
   console.log(feedback);
 
   function getLabelText(value) {
-    return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+    return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
   }
 
-  
   const getUnpaidbills = async () => {
     const reqHeader = {
       "Content-Type": "application/json",
-      "Authorization": `Token ${token}`,
+      Authorization: `Token ${token}`,
     };
     try {
       // Add authorization header
-      const result = await getUnpaidbillsApi(reqHeader)
-      console.log(result);
+      const result = await axios.get(
+        `${baseurl}/maternalcare/primarybooking/billunpaid/view`,
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+      console.log(result.data);
+      setUnpaid(result?.data?.bill);
     } catch (error) {
-      console.error('Error fetching unpaid bills:', error);
+      console.error("Error fetching unpaid bills:", error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    getUnpaidbills()
-  },[])
+  useEffect(() => {
+    getUnpaidbills();
+  }, []);
   return (
     <>
       <Header />
-      <Container style={{ marginTop: '50px' }}>
-        <h1 className='text-center mb-5'>Booking Details</h1>
-        <Col md={4} className='d-flex justify-content-center align-items-center' style={{ width: '400px', height: '400px' }}>
-          <div style={{ width: '400px', height: 'auto' }} className='div2 text-center mb-5 p-3'>
-            <div className='mt-5 text-light'>
-              <p>Dr Name : Aryamol</p>
-              <p>Service : Emergency</p>
-              <p>Booking Date : 05/04/2024</p>
-              <p>Booking Time : 05:30</p>
-              <p>Rate : 5000</p>
-              <p>Action : Approved</p>
-              <Button onClick={handleOpen}>Feedback</Button>
-              <Payment />
-            </div>
-          </div>
-        </Col>
+      <Container style={{ marginTop: "50px" }}>
+        <Row>
+          <h1 className="text-center mb-5">Booking Details</h1>
+          {unpaid.length > 0
+            ? unpaid.map((i) => (
+                <Col
+                  md={4}
+                  className=""
+                  style={{ width: "400px", height: "400px" }}
+                >
+                  <div
+                    style={{ height: "380px" }}
+                    className="div2 text-center mb-5 "
+                  >
+                    <div className="mt-5 text-light p-2">
+                      <p>Name : {i?.userName.toUpperCase()}</p>
+                      <p>Working Hours : {i?.workinghours}</p>
+                      <p>Start Date : {i?.startDate}</p>
+                      <p>Starting Time : {i?.startingTime.slice(0, 6)}</p>
+                      <p>Rate : {i?.rate}</p>
+                      <p>Total : {i?.amountPaid}</p>
+                      <p>Action : Approved</p>
+                      <Button >Feedback</Button>
+                      <Payment item = {i}/>
+                    </div>
+                  </div>
+                </Col>
+              ))
+            : null}
+        </Row>
       </Container>
 
       {/* Modal */}
@@ -93,7 +123,12 @@ function MyBookings() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2" className='mb-3'>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            className="mb-3"
+          >
             Add Your Feedback
           </Typography>
           <Typography id="modal-modal-description" xl={{ mt: 2 }}>
@@ -112,13 +147,26 @@ function MyBookings() {
                 size="large"
               />
               {feedback.rating !== null && (
-                <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : feedback.rating]}</Box>
+                <Box sx={{ ml: 2 }}>
+                  {labels[hover !== -1 ? hover : feedback.rating]}
+                </Box>
               )}
             </Stack>
-            <TextField style={{ width: '100%' }} id="outlined-multiline-static" label="Multiline" multiline rows={5} placeholder="Enter your feedback" value={feedback.comments} onChange={e => setFeedback({ ...feedback, comments: e.target.value })} />
+            <TextField
+              style={{ width: "100%" }}
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              rows={5}
+              placeholder="Enter your feedback"
+              value={feedback.comments}
+              onChange={(e) =>
+                setFeedback({ ...feedback, comments: e.target.value })
+              }
+            />
           </Typography>
           <div className="d-flex justify-content-center">
-            <button className='btn btn-outline-success mt-3' >Submit</button>
+            <button className="btn btn-outline-success mt-3">Submit</button>
           </div>
         </Box>
       </Modal>
