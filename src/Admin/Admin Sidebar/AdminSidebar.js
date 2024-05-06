1d20babdda1a62b78a159e21b5ca10675ba86784
midhun2208 <midhun.sm343@gmail.com>
2024-05-06 13:11:31 +0530

    Midhun P M

--- src/Admin/Admin Sidebar/AdminSidebar.js
+++ src/Admin/Admin Sidebar/AdminSidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -6,108 +6,141 @@ import {
   CDBSidebarHeader,
   CDBSidebarMenu,
   CDBSidebarMenuItem,
-} from 'cdbreact';
-import { NavLink } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import {useNavigate} from 'react-router-dom'
+} from "cdbreact";
+import { NavLink } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function AdminSidebar() {
-const navigate=useNavigate()
-  const logout=()=>{
-    sessionStorage.clear()
-    navigate('/')
+  const navigate = useNavigate();
+  const logout = () => {
+    sessionStorage.clear();
+    navigate("/");
+    Swal.fire({
+      title: "Logged out",
+      icon: "success",
+    });
+  };
 
-  }
- 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', overflow: 'scroll initial' }}>
-      
-      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#9C6644" }}>
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto">
-      <div className="dropdown">
-    </div>
-       
-  
-        <li className="nav-item mx-2"  >
-        <a className="nav-link" href="/admin-chat">
-        <i className="fa fa-envelope" style={{ color: 'white' }} title="Chat"     ></i>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        overflow: "scroll initial",
+      }}
+    >
+      <nav
+        className="navbar navbar-expand-lg"
+        style={{ backgroundColor: "#9C6644" }}
+      >
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <div className="dropdown"></div>
 
-          </a> 
-        </li>
-        <li> 
+              <li className="nav-item mx-2">
+                <a className="nav-link" href="/admin-chat">
+                  <i
+                    className="fa fa-envelope"
+                    style={{ color: "white" }}
+                    title="Chat"
+                  ></i>
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" href="#">
+                  <i
+                    className="fa fa-power-off"
+                    style={{ color: "white", marginRight: "5px" }}
+                    onClick={logout}
+                    title="Log Out"
+                  ></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-        <a className="nav-link" href="#">
-      <i className="fa fa-power-off" style={{ color: 'white', marginRight: '5px' }} onClick={logout} title="Log Out"></i>
-    </a>
-        </li>
-      
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-
-     
-      <CDBSidebar textColor="#fff" style={{ background: 'linear-gradient(to bottom, #9C6644, #DDB892)' }}>
-        <CDBSidebarHeader >
-          <a className="text-decoration-none" style={{ color: 'inherit' }}>
+      <CDBSidebar
+        textColor="#fff"
+        style={{ background: "linear-gradient(to bottom, #9C6644, #DDB892)" }}
+      >
+        <CDBSidebarHeader>
+          <a className="text-decoration-none" style={{ color: "inherit" }}>
             <Stack direction="row" spacing={2}>
               <Avatar
                 alt="Cindy Baker"
                 src="https://i.postimg.cc/BbqPkcnD/360-F-65756860-GUZwz-OKNMUU3-Hld-Fo-IA44qss7-ZIr-CG8-I.jpg"
-                style={{ width: '50px', height: '50px' }}
+                style={{ width: "50px", height: "50px" }}
               />
-              <h6 className='mt-4 ms-2'>Admin </h6>
+              <h6 className="mt-4 ms-2">Admin </h6>
             </Stack>
           </a>
         </CDBSidebarHeader>
 
-        <CDBSidebarContent  >
+        <CDBSidebarContent>
           <CDBSidebarMenu>
             <NavLink exact to="/admin-dash" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="home">Dashboard</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/admin-leave" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="note-sticky">Leave Requests</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="note-sticky">
+                Leave Requests
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/admin-bookings" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="book">Bookings</CDBSidebarMenuItem>
             </NavLink>
-           
+
             <NavLink exact to="/admin-blog" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="chart-line">Blogs</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/admin-approval" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="hand">Approval Request</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="hand">
+                Approval Request
+              </CDBSidebarMenuItem>
             </NavLink>
-            
-            <NavLink exact to="/admin-webinar"  activeClassName="activeClicked">
+
+            <NavLink exact to="/admin-webinar" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="handshake">Webinar</CDBSidebarMenuItem>
             </NavLink>
             {/* <NavLink exact to="/add-category"  activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="list">Categories</CDBSidebarMenuItem>
             </NavLink> */}
-            <NavLink exact to="/add-emergency"  activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="tower-broadcast">Emergency</CDBSidebarMenuItem>
+            <NavLink exact to="/add-emergency" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="tower-broadcast">
+                Emergency
+              </CDBSidebarMenuItem>
             </NavLink>
-          
           </CDBSidebarMenu>
         </CDBSidebarContent>
 
-        <CDBSidebarFooter style={{ textAlign: 'center' }}>
-          <div style={{ padding: '20px 100px' }}>
-          <Stack direction="row" spacing={2}>
-          <Avatar onClick={logout} style={{ color: "white", backgroundColor:"#9C6644"}}>L</Avatar>
-      
-    </Stack>
-          
+        <CDBSidebarFooter style={{ textAlign: "center" }}>
+          <div style={{ padding: "20px 100px" }}>
+            <Stack direction="row" spacing={2}>
+              <Avatar
+                onClick={logout}
+                style={{ color: "white", backgroundColor: "#9C6644" }}
+              >
+                L
+              </Avatar>
+            </Stack>
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
--- src/ServiceProvider/Bookings/AcceptedBookings.jsx
+++ src/ServiceProvider/Bookings/AcceptedBookings.jsx
@@ -1,66 +1,97 @@
-import { Container } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { getAllAcceptedBookings } from '../../Services/allApi';
-import HeaderSP from '../Navbar/HeaderSP';
+import { Container } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { getAllAcceptedBookings } from "../../Services/allApi";
+import HeaderSP from "../Navbar/HeaderSP";
 
 function AcceptedBookings() {
-
-  const [accepted,setAccepted] = useState([])
+  const [accepted, setAccepted] = useState([]);
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
-  }
+  };
 
   const getAcceptedBookings = async () => {
     const result = await getAllAcceptedBookings();
-    const formattedBookings = result.data.acceptedBookings.map(booking => ({
+    const formattedBookings = result.data.acceptedBookings.map((booking) => ({
       ...booking,
       startDate: formatDate(booking.startDate),
-      endDate: formatDate(booking.endDate)
+      endDate: formatDate(booking.endDate),
     }));
     setAccepted(formattedBookings);
-  }
+  };
 
   useEffect(() => {
     getAcceptedBookings();
   }, []);
+  console.log(accepted);
 
   return (
     <>
-      <HeaderSP/>
-      <Container className='mt-5'>
-        <h1 className='text-center text-primary'>Accepted Bookings</h1>
-       <div className='row'>
+      <HeaderSP />
+      <Container className="mt-5">
+        <h1 className="text-center text-primary">Accepted Bookings</h1>
+        <div className="row">
           {accepted?.length > 0 ? (
             accepted.map((item) => (
-              <div className="card2 card col-md-4 mt-5 text-center shadow p-2 m-3 ms-5" style={{ width: '18rem',backgroundColor:'' }} key={item.id}>
-                <ul className="list-group list-group-light list-group-small">
-                  <li className="list-group-item px-3">Username : {item.userName}</li>
-                  <li className="list-group-item px-3">Service Provider Name : {item.serviceProviderName}</li>
-                  <li className="list-group-item px-3">Service : {item.services}</li>
-                  <li className="list-group-item px-3">Type of Care : {item.typeOfCare}</li>
-                  <li className="list-group-item px-3">Start Date : {item.startDate}</li>
-                  <li className="list-group-item px-3">End Date : {item.endDate}</li>
-                  <li className="list-group-item px-3">Starting Time : {item.startingTime}</li>
-                  <li className="list-group-item px-3">Ending Time : {item.endingTime}</li>
-                  <li className="list-group-item px-3">Location : {item.location}</li>
-                  <li className="list-group-item px-3">Rate : {item.rate}</li>
-                  <li className="list-group-item px-3">Service Provider Status : {item.serviceProviderStatus}</li>
-                  <li className="list-group-item px-3">Admin Status : {item.adminStatus}</li>
-                </ul>
+              <div className="col-lg-4">
+                <div
+                  className="card2  mt-5 text-center shadow p-2 "
+                  style={{ width: "", backgroundColor: "" }}
+                  key={item.id}
+                >
+                  <ul className="list-group list-group-light list-group-small">
+                    <li className="list-group-item px-3">
+                      Username : {item.userName}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Service Provider Name : {item.serviceProviderName}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Service : {item.services}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Type of Care : {item.typeOfCare}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Start Date : {item.startDate}
+                    </li>
+                    <li className="list-group-item px-3">
+                      End Date : {item.endDate}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Starting Time : {item.startingTime}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Ending Time : {item.endingTime}
+                    </li>
+                    <li className="list-group-item px-3">
+                      Location : {item.location}
+                    </li>
+                    <li className="list-group-item px-3">Rate : {item.rate}</li>
+                    <li className="list-group-item px-3">
+                      Service Provider Status : {item.serviceProviderStatus} ✅
+                    </li>
+                    <li className="list-group-item px-3">
+                      Admin Status :{" "}
+                      {item?.adminStatus === "approved" ? "Accepted ✅" : "Decliend ❌"}
+                    </li>
+                  </ul>
+                </div>
               </div>
             ))
           ) : (
-            <h1 className='text-center text-danger'>No Accepted Booking Available</h1>  
+            <h1 className="text-center text-danger">
+              No Accepted Booking Available
+            </h1>
           )}
-       </div>
+        </div>
       </Container>
     </>
-  )
+  );
 }
 
 export default AcceptedBookings;
